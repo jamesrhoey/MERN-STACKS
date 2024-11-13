68619ed325db78b9d2fd22a6576de7cd77ddd648
jamesrhoey <jamesrhoeydecastro7@gmail.com>
2024-11-13 15:09:41 +0800

    mern

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+
 
 const app = express()
 
 
-app.use(express.json())
+app.use(express.json)
 app.use((req, res, next) =>{
     console.log(req.path, req.method)
     next()
@@ -14,8 +16,15 @@ app.use((req, res, next) =>{
 
 app.use('/api/workouts', workoutRoutes)
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() =>{
+        app.listen(process.env.PORT, () => {
+            console.log('Listening in port', process.env.PORT)
+        })
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
 
-app.listen(process.env.PORT, () => {
-    console.log('Listening in port', process.env.PORT)
-})
+
 
